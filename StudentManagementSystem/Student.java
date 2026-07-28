20400b60bbec2f357614c02b626ca20b51213e76
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-07-29 00:35:28 +0530

    Add Student class with methods for info and result

--- StudentManagementSystem/Student.java
+++ StudentManagementSystem/Student.java
@@ -1,1 +1,28 @@
 
+public class Student {
+    String Student_id;
+    String name;
+    int age;
+    String course;
+    float marks; 
+    void info(){
+        System.out.println("Student id:"+ Student_id);
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+ age);
+        System.out.println("Course: "+ course);
+        System.out.println("Makrs: "+marks);
+        System.out.println("--------");
+    }
+    void update_marks(float a){
+        marks=a;
+        System.out.println("marks update:"+ marks);
+    }
+    void result(){
+        if(marks>=33){
+            System.out.println("Pass");
+        }
+        else{
+            System.out.println("Fail");
+        }
+    }
+}
