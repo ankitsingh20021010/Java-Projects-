8a9f1c53d9fa73dcc0411386f5c475944f296548
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-07-29 00:11:16 +0530

    Implement account management methods in Account class
    Added methods for withdrawal, deposit, balance check, and account information display.

--- BankingSystem/Account.java
+++ BankingSystem/Account.java
@@ -1,1 +1,35 @@
+    public class Account {
+        String account_number;
+        long phone_number;
+        String type=" Saving";
+        double balanace;
+        void Withdrawl(double amount){
+            if(amount<= balanace){
+        balanace=balanace - amount;
+        System.out.println("withdrawl successfull!");
+        System.out.println("current balance is: "+ balanace);
+            }
+            else{
+    System.out.println("Insufficient Balance");
+}
+            System.out.println("------------");
+        }
+        void deposit(double amount){
+        if(amount>0){
+            balanace+=amount;
+            System.out.println("Balance Deposit");
+            System.out.println("current balace is :"+ balanace);
+        }
+        }
+        void checkbalance(){
+            System.out.println("balance is :"+ balanace);
+        }
+        void account_info(){
+       System.err.println("Account number : "+ account_number);            
+       System.out.println("Phone no:- "+ phone_number);
+       System.out.println("Account type "+ type);
+       System.out.println("Balance:"+ balanace);
+       System.out.println("----------------");
 
+        }
+    }
