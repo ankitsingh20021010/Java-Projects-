767e4ff838443ce56c94387db8a539ba0d18b16d
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-07-29 00:35:03 +0530

    Implement main method for Student management
    version 1

--- StudentManagementSystem/Main.java
+++ StudentManagementSystem/Main.java
@@ -1,1 +1,13 @@
-
+public class Main{
+    public static void main(String[] args){
+        Student s1=new Student();
+        s1.Student_id="008086";
+        s1.course="BCA DS & AI";
+        s1.name="Ankit Kumar";
+        s1.age=22;
+        s1.marks=65;
+        s1.info();
+        s1.update_marks(78.5F);
+        s1.result();
+    }
+}
