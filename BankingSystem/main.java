f2e416410d8909fce1585ea54c9542b89e13bc7c
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-07-29 00:10:43 +0530

    Implement main method for BankingSystem
    Added main method to initialize accounts and perform operations.

--- BankingSystem/main.java
+++ BankingSystem/main.java
@@ -1,1 +1,19 @@
 
+import java.util.*;
+
+class main{
+    public static void main(String[] args) {
+       System.out.println("code run successfully"); 
+       Scanner sc=new Scanner(System.in);
+       Account a1=new Account();
+       Account a2=new Account();
+       Account a3=new Account();
+       a1.account_number="6392887349";
+       a1.phone_number=638843615L;
+       a1.balanace=100;
+       a1.account_info();
+       a1.Withdrawl(700);
+       a1.checkbalance();
+       a1.deposit(10000);
+    }
+}
