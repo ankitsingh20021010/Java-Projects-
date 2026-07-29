fed742d4950b3d7869dd60443894af90b6be851f
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-07-29 12:49:20 +0530

    Update Main.java
    vesion 1

--- MobileStore/Main.java
+++ MobileStore/Main.java
@@ -1,1 +1,23 @@
-
+class Main{
+ public static void main(String[] args) {
+    Mobile m1 = new Mobile(
+    "Samsung",
+    "S24",
+    8,
+    256,
+    5000,
+    74999,
+    10
+);
+m1.MobileInfo();
+Mobile m2=new Mobile("Apple","SE",6,112,4200,45000,20);
+ m2.MobileInfo();
+ Mobile m3=new Mobile("vivo","v20",6,112,5000,15000,25);
+ m3.MobileInfo();
+ m1.checkStock();
+ Customer c1 = new Customer(1,"Ankit","9876543210","Lucknow");
+c1.buyMobile(m1);
+ m1.checkStock();
+ c1.customerInfo();
+ }
+}
